0313e76f677f169f21f837dac453448358c1a3b9
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2023-07-18 19:56:14 +0530

    Update data.js

--- static/data.js
+++ static/data.js
@@ -1,11 +1,4 @@
 export const data = {
-    stories: [
-
-        {
-            username: 'mrbeast',
-            image:
-                'https://yt3.googleusercontent.com/ytc/AMLnZu9UlPHTbLrgfwt0ucUKik9Z8DfO6dVgVnfum2o9hA=s176-c-k-c0x00ffffff-no-rj-mo',
-        },
-    ],
+    
     
 }
